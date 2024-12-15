30fa7fc76bf7930e7c011354fd8a90b255eac7bd
Emi01931 <75816136+Emi01931@users.noreply.github.com>
2024-12-14 21:47:09 -0600

    Diseño del menu principal

---
+++ ProyectoFinal/Assets/Scripts/BtnStartScript.cs
@@ -0,0 +1,39 @@
+// Created by NightTime Developments!
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Mainmenu : MonoBehaviour
+{
+
+    public GameObject mainmenue;
+    public GameObject Player;
+    public GameObject MenuCam;
+
+    // Start is called before the first frame update
+
+    public void Start(){
+        Player.SetActive(false);
+        mainmenue.SetActive(true);
+    }
+
+
+    // Update is called once per frame
+
+    /* void Update(){
+     
+     }*/
+
+    public void StartButton(){
+        mainmenue.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        Player.SetActive(true);
+        MenuCam.SetActive(false);
+    }
+
+}
+
+
